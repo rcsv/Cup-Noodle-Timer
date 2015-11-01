388b5798b722e1b19da9a1ae4e8906ead68bb703
rcsv <Awane.Tomohiro@gmail.com>
2015-11-01 22:17:49 +0900

    fixed unary operator in 42.

--- timer.sh
+++ timer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ESC="\033"
 
 Usage () {
   # Name:     usage - display simple usage
@@ -8,23 +9,38 @@ Usage () {
   echo "usage: ${cmd_name} <minutes>";
 }
 
+CheckOS () {
+  if [ "$(uname)" == 'Darwin' ]; then
+    OS='Mac'
+  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    OS='Linux'
+  elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+    OS='Cygwin'
+  else
+    OS='UNKNOWN'
+  fi
+}
+
 ((!$#)) && echo "Please specify minutes" 1>&2 && Usage && exit 1
 expr "$1" + 1 >/dev/null 2>&1
 if [ $? -ge 2 ]; then
   echo "Please speficy a number" 1>&2 && Usage && exit 1
 fi
 
+CheckOS
+if [ $OS == 'UNKNOWN' ]; then
+  echo "($(uname)) is not supported." && exit 2
+fi
 
-sec=`expr $1 \* 60 -3`
-echo 'start'
 
-say Timer starts right now.
+sec=`expr $1 \* 60 - 3`
+echo "timer start:"
+[ $OS == 'Mac' ] && say Timer starts.
 
 while [ $sec != 0 ] ; do
   sleep 1
-  sec=`expr $sec -1`
+  sec=`expr $sec - 1`
 done
-
 sec=3
 
 until [ $sec = 0 ] ; do
